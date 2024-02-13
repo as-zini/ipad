1b8d4e639605ae4a19006b0a6dba09605e31f944
as-zini <aszini@naver.com>
2024-02-13 22:07:41 +0900

    반응형 레이아웃 작업

--- js/main.js
+++ js/main.js
@@ -44,13 +44,17 @@ const searchDelayEls = [...searchWrapEl.querySelectorAll('li')]
 
 searchStarterEl.addEventListener('click', showSearch) //익명함수대신 실행할 이름이 있는 함수이름을 넣어줘도됨
 
-searchCloserEl.addEventListener('click', hideSearch)
+searchCloserEl.addEventListener('click', function(event){
+  //기존에는 이 버튼을 누르면 결국 이벤트버블링으로 부모요소인 검색필드까지 눌려 취소 버튼을 누른후 모바일모드가 변경되었을때 검색창이 먼저 뜨게됨 -> 이벤트버블링을 없애서 검색필드를 누른것과 같은 효과를 방지
+  event.stopPropagation()
+  hideSearch()
+})
 
 searchShadowEl.addEventListener('click', hideSearch)
 
 function showSearch(){
   headerEl.classList.add('searching')
-  document.documentElement.classList.add('fixed') //documentElement는 html 전체를 의미
+  stopScroll() //documentElement는 html 전체를 의미
   headerMenuEls.reverse().forEach(function(el, index){ //헤더메뉴 순차적으로 사라지는 애니메이션 적용
     el.style.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
@@ -64,7 +68,7 @@ function showSearch(){
 
 function hideSearch(){
   headerEl.classList.remove('searching')
-  document.documentElement.classList.remove('fixed')
+  playScroll()
   headerMenuEls.reverse().forEach(function(el, index){
     el.computedStyleMap.transitionDelay = index * .4 / headerMenuEls.length + 's'
   })
@@ -75,6 +79,76 @@ function hideSearch(){
   searchInputEl.value = '' //검색바를 닫으면 입력한 검색어가 초기화되는 코드
 }
 
+function playScroll(){
+  document.documentElement.classList.remove('fixed')
+}
+
+function stopScroll(){
+  document.documentElement.classList.add('fixed')
+}
+
+
+// 모바일 모드에서 헤더 메뉴 토글
+const menuStarterEl = document.querySelector('header .menu-starter')
+menuStarterEl.addEventListener('click',function(){
+  if(headerEl.classList.contains('menuing')){
+    headerEl.classList.remove('menuing')
+    searchInputEl.value = ''
+    playScroll()
+  } else{
+    headerEl.classList.add('menuing')
+    stopScroll()
+  }
+  
+})
+
+//모바일 모드에서 헤더 검색!
+const searchTextFieldEl = document.querySelector('header .textfield')
+const serachCancelEl = document.querySelector('header .search-canceler')
+searchTextFieldEl.addEventListener('click', function(){
+  headerEl.classList.add('searching--mobile')
+  searchInputEl.focus()
+})
+serachCancelEl.addEventListener('click', function(){
+  headerEl.classList.remove('searching--mobile')
+})
+
+
+// 데스크탑 모드에서 검색을 누른채로 화면 크기를 줄이면 searching 클래스가 그대로 남아 의도하지 않은 채로 동작하는 것을 막는 코드
+window.addEventListener('resize', function (){ /*resize 이벤트는 화면의 크기가 변할때 함수실행*/
+  if(this.window.innerWidth <= 740){ /*innerWidth는 화면크기의 변화를 의미*/
+    headerEl.classList.remove('searching')
+  } else{
+    headerEl.classList.remove('searching--mobile')
+  }
+})
+
+const navEl = document.querySelector('nav')
+const navMenuToggleEl = navEl.querySelector('.menu-toggler')
+const navMenuShadowEl = navEl.querySelector('.shadow')
+
+navMenuToggleEl.addEventListener('click', function(){
+  if(navEl.classList.contains('menuing')){
+    hideNavMenu()
+  } else {
+    showNavMenu()
+  }
+})
+navEl.addEventListener('click',function(event){
+  event.stopPropagation()
+})
+navMenuShadowEl.addEventListener('click', hideNavMenu)
+
+window.addEventListener('click', hideNavMenu)
+
+function showNavMenu(){
+  navEl.classList.add('menuing')
+}
+
+
+function hideNavMenu(){
+  navEl.classList.remove('menuing')
+}
 
 // 요소가 화면에 보이는지 관찰
  //요소가 화면에 관찰되면 실행할 함수
@@ -158,6 +232,7 @@ navigations.forEach(function(nav){
   mapEl.innerHTML = /* HTML */ `
     <h3>
       <span class="text">${nav.title}</span>
+      <span class="icon">+</span>
     </h3>
     <ul>
       ${mapList}
@@ -172,3 +247,12 @@ navigations.forEach(function(nav){
 const thisYearEl = document.querySelector('span.this-year')
 thisYearEl.textContent = new Date().getFullYear()
 
+
+// Mobile Footer animation
+const mapEls = document.querySelectorAll('footer .navigations .map')
+mapEls.forEach(function(el){
+  const h3El = el.querySelector('h3')
+  h3El.addEventListener('click', function(){
+    el.classList.toggle('active')  //toggle메소드는 입력된 클래스가 있을때는 제거해주고 없을때는 추가해줌
+  })
+})
